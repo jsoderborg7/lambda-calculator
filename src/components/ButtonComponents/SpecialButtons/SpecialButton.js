786ddd6c62280bc6c89f7c2351fa4c704e61d9b8
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-13 14:03:31 -0600

    specials and specialButton done

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
-import Specials from "./Specials";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const {specials} = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{specials}</button>
     </>
   );
 };
+
+export default SpecialButton;
